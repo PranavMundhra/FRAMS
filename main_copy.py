0f0316b11793d3aa193d78f8a6f7ab513c9c471f
PranavMundhra <50544179+PranavMundhra@users.noreply.github.com>
2024-10-03 11:09:45 +0530

    Update main_copy.py

--- main_copy.py
+++ main_copy.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import subprocess
+from datetime import datetime
+import os
 
 def main():
     st.title("Attendance System")
@@ -14,13 +16,31 @@ def main():
 
 def collect_data():
     try:
-        subprocess.run(["python", "data.py"])
+        today = datetime.now()
+        month_year = today.strftime("%B_%Y")
+        excel_file = f"data/attendance_{month_year}.xlsx"
+
+        # Check if the Excel file exists
+        if not os.path.exists(excel_file):
+            print(f"Excel file '{excel_file}' not found. Running test.py...")
+            subprocess.run(["python", "excel.py"])  
+        
+        subprocess.run(["python", "collect_data.py"])
         st.success("Data collection completed.")
     except Exception as e:
         st.error(f"Failed to execute collect_data.py: {e}")
 
 def mark_attendance():
     try:
+        today = datetime.now()
+        month_year = today.strftime("%B_%Y")
+        excel_file = f"data/attendance_{month_year}.xlsx"
+
+        # Check if the Excel file exists
+        if not os.path.exists(excel_file):
+            print(f"Excel file '{excel_file}' not found. Running test.py...")
+            subprocess.run(["python", "excel.py"])  
+            
         subprocess.run(["python", "mark_attendance.py"])
         st.success("Attendance marked successfully.")
     except Exception as e:
